5848a1365653cf897d7637fca80d59a21f949be2
xiaomo <xiaomo@mahua.com>
2021-09-17 14:51:16 +0800

    second commit

---
+++ src/main/java/com/example/mydocker/controller/BusinessController.java
@@ -0,0 +1,13 @@
+package com.example.mydocker.controller;
+
+/**
+ * @author 小墨
+ * @version 1.0
+ * @description TODO
+ * @date 2021/9/17 14:48
+ **/
+public class BusinessController {
+    public static void main(String[] args) {
+        System.out.println("ddhhd");
+    }
+}
